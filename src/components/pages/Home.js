e490ba9ad725492cef7463713088f2080365f995
reubenjamesbishop <reubenjamesbishop@gmail.com>
2021-11-02 16:34:26 +1030

    2 files modified, 5 added

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -1,7 +1,28 @@
 import React from 'react';
+// import { Bar } from 'react-chartjs-2';
+import VerticalBar from '../graphs/VerticalBar';
+import Doughnut from '../graphs/Doughnut';
+import HorizontalBar from '../graphs/HorizontalBar';
+import Radar from '../graphs/Radar';
 
 function Home() {
-  return <p>Home section from Home.js</p>;
+  return (
+    <>
+      <h1>home</h1>
+      <div class="container">
+        <div class="row">
+          <div class="col-6">
+            <VerticalBar />
+            <Doughnut />
+          </div>
+          <div class="col-6">
+            <HorizontalBar />
+            <Radar />
+          </div>
+        </div>
+      </div>
+    </>
+  );
 }
 
 export default Home;
