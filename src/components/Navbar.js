d24562f0caa197382e2974916a4df71a984d20da
reubenjamesbishop <reubenjamesbishop@gmail.com>
2021-11-08 16:22:18 +1030

    1 file modified

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -50,17 +50,6 @@ function Navbar() {
               </a>
             </li>
           </ul>
-          <form class="form-inline">
-            <input
-              class="form-control mr-sm-2"
-              type="search"
-              placeholder="Search"
-              aria-label="Search"
-            />
-            <button class="btn btn-outline-primary my-2 my-sm-0" type="submit">
-              Search
-            </button>
-          </form>
         </div>
       </nav>
     </>
