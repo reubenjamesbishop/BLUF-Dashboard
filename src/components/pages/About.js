1aed29e9da54dca8ce75d2a2a12d3ab6d8f48464
reubenjamesbishop <reubenjamesbishop@gmail.com>
2021-11-08 15:07:44 +1030

    3 files modified

--- src/components/pages/About.js
+++ src/components/pages/About.js
@@ -16,17 +16,17 @@ function About() {
             prepare and can only use a whiteboard marker to assist.
           </p>
           <p class="about-paragraph">
-          <strong>BLUF</strong> presentations have a significantly larger emphasis on the
-            professional development of the presenter than the technical
-            information presented. This focuses on polishing valuable skills for
-            future professional presentations and creates a more casual
-            atmosphere to encourages audience interaction. actually sorry maybe
-            this is better
+            <strong>BLUF</strong> presentations have a significantly larger
+            emphasis on the professional development of the presenter than the
+            technical information presented. This focuses on polishing valuable
+            skills for future professional presentations and creates a more
+            casual atmosphere to encourages audience interaction. actually sorry
+            maybe this is better
           </p>
           <p class="about-paragraph">
-          <strong>BLUF</strong> presentations can be used to test your presentation skills by
-            presenting on a topic in which you have little prior knowledge and
-            limited time to prepare. 
+            <strong>BLUF</strong> presentations can be used to test your
+            presentation skills by presenting on a topic in which you have
+            little prior knowledge and limited time to prepare. 
           </p>
           <p class="about-paragraph">
             Presenting in such situations can be daunting but can quickly
@@ -46,55 +46,30 @@ function About() {
           </footer>
         </text>
       </blockquote>
+
+      {/* ///////////////////////////////  FAQ   ///////////////////////////////////////////////*/}
+
       <h1 class="FAQ-header">FAQ</h1>
-      <div class="accordion" id="accordionPanelsStayOpenExample">
+
+      <div class="accordion" id="accordionExample">
         <div class="accordion-item">
-          <h2 class="accordion-header" id="panelsStayOpen-headingOne">
-            <button
-              class="accordion-button"
-              type="button"
-              data-bs-toggle="collapse"
-              data-bs-target="#panelsStayOpen-collapseOne"
-              aria-expanded="true"
-              aria-controls="panelsStayOpen-collapseOne"
-            >
-              Accordion Item #1
-            </button>
-          </h2>
-          <div
-            id="panelsStayOpen-collapseOne"
-            class="accordion-collapse collapse show"
-            aria-labelledby="panelsStayOpen-headingOne"
-          >
-            <div class="accordion-body">
-              <strong>This is the first item's accordion body.</strong> It is
-              shown by default, until the collapse plugin adds the appropriate
-              classes that we use to style each element. These classes control
-              the overall appearance, as well as the showing and hiding via CSS
-              transitions. You can modify any of this with custom CSS or
-              overriding our default variables. It's also worth noting that just
-              about any HTML can go within the <code>.accordion-body</code>,
-              though the transition does limit overflow.
-            </div>
-          </div>
-        </div>
-        <div class="accordion-item">
-          <h2 class="accordion-header" id="panelsStayOpen-headingTwo">
+          <h2 class="accordion-header" id="headingTwo">
             <button
               class="accordion-button collapsed"
               type="button"
               data-bs-toggle="collapse"
-              data-bs-target="#panelsStayOpen-collapseTwo"
+              data-bs-target="#collapseTwo"
               aria-expanded="false"
-              aria-controls="panelsStayOpen-collapseTwo"
+              aria-controls="collapseTwo"
             >
               Accordion Item #2
             </button>
           </h2>
           <div
-            id="panelsStayOpen-collapseTwo"
+            id="collapseTwo"
             class="accordion-collapse collapse"
-            aria-labelledby="panelsStayOpen-headingTwo"
+            aria-labelledby="headingTwo"
+            data-bs-parent="#accordionExample"
           >
             <div class="accordion-body">
               <strong>This is the second item's accordion body.</strong> It is
@@ -109,22 +84,147 @@ function About() {
           </div>
         </div>
         <div class="accordion-item">
-          <h2 class="accordion-header" id="panelsStayOpen-headingThree">
+          <h2 class="accordion-header" id="headingThree">
             <button
               class="accordion-button collapsed"
               type="button"
               data-bs-toggle="collapse"
-              data-bs-target="#panelsStayOpen-collapseThree"
+              data-bs-target="#collapseThree"
               aria-expanded="false"
-              aria-controls="panelsStayOpen-collapseThree"
+              aria-controls="collapseThree"
             >
               Accordion Item #3
             </button>
           </h2>
           <div
-            id="panelsStayOpen-collapseThree"
+            id="collapseThree"
             class="accordion-collapse collapse"
-            aria-labelledby="panelsStayOpen-headingThree"
+            aria-labelledby="headingThree"
+            data-bs-parent="#accordionExample"
+          >
+            <div class="accordion-body">
+              <strong>This is the third item's accordion body.</strong> It is
+              hidden by default, until the collapse plugin adds the appropriate
+              classes that we use to style each element. These classes control
+              the overall appearance, as well as the showing and hiding via CSS
+              transitions. You can modify any of this with custom CSS or
+              overriding our default variables. It's also worth noting that just
+              about any HTML can go within the <code>.accordion-body</code>,
+              though the transition does limit overflow.
+            </div>
+          </div>
+        </div>
+        <div class="accordion-item">
+          <h2 class="accordion-header" id="headingThree">
+            <button
+              class="accordion-button collapsed"
+              type="button"
+              data-bs-toggle="collapse"
+              data-bs-target="#collapseThree"
+              aria-expanded="false"
+              aria-controls="collapseThree"
+            >
+              Accordion Item #3
+            </button>
+          </h2>
+          <div
+            id="collapseThree"
+            class="accordion-collapse collapse"
+            aria-labelledby="headingThree"
+            data-bs-parent="#accordionExample"
+          >
+            <div class="accordion-body">
+              <strong>This is the third item's accordion body.</strong> It is
+              hidden by default, until the collapse plugin adds the appropriate
+              classes that we use to style each element. These classes control
+              the overall appearance, as well as the showing and hiding via CSS
+              transitions. You can modify any of this with custom CSS or
+              overriding our default variables. It's also worth noting that just
+              about any HTML can go within the <code>.accordion-body</code>,
+              though the transition does limit overflow.
+            </div>
+          </div>
+        </div>
+        <div class="accordion-item">
+          <h2 class="accordion-header" id="headingTwo">
+            <button
+              class="accordion-button collapsed"
+              type="button"
+              data-bs-toggle="collapse"
+              data-bs-target="#collapseTwo"
+              aria-expanded="false"
+              aria-controls="collapseTwo"
+            >
+              Accordion Item #2
+            </button>
+          </h2>
+          <div
+            id="collapseTwo"
+            class="accordion-collapse collapse"
+            aria-labelledby="headingTwo"
+            data-bs-parent="#accordionExample"
+          >
+            <div class="accordion-body">
+              <strong>This is the second item's accordion body.</strong> It is
+              hidden by default, until the collapse plugin adds the appropriate
+              classes that we use to style each element. These classes control
+              the overall appearance, as well as the showing and hiding via CSS
+              transitions. You can modify any of this with custom CSS or
+              overriding our default variables. It's also worth noting that just
+              about any HTML can go within the <code>.accordion-body</code>,
+              though the transition does limit overflow.
+            </div>
+          </div>
+        </div>
+        <div class="accordion-item">
+          <h2 class="accordion-header" id="headingThree">
+            <button
+              class="accordion-button collapsed"
+              type="button"
+              data-bs-toggle="collapse"
+              data-bs-target="#collapseThree"
+              aria-expanded="false"
+              aria-controls="collapseThree"
+            >
+              Accordion Item #3
+            </button>
+          </h2>
+          <div
+            id="collapseThree"
+            class="accordion-collapse collapse"
+            aria-labelledby="headingThree"
+            data-bs-parent="#accordionExample"
+          >
+            <div class="accordion-body">
+              <strong>This is the third item's accordion body.</strong> It is
+              hidden by default, until the collapse plugin adds the appropriate
+              classes that we use to style each element. These classes control
+              the overall appearance, as well as the showing and hiding via CSS
+              transitions. You can modify any of this with custom CSS or
+              overriding our default variables. It's also worth noting that just
+              about any HTML can go within the <code>.accordion-body</code>,
+              though the transition does limit overflow.
+            </div>
+          </div>
+        </div>
+        <div class="accordion-item">
+          <h2 class="accordion-header" id="headingThree">
+            <button
+              class="accordion-button collapsed"
+              type="button"
+              data-bs-toggle="collapse"
+              data-bs-target="#collapseThree"
+              aria-expanded="false"
+              aria-controls="collapseThree"
+            >
+              Accordion Item #3
+            </button>
+          </h2>
+          <div
+            id="collapseThree"
+            class="accordion-collapse collapse"
+            aria-labelledby="headingThree"
+            data-bs-parent="#accordionExample"
           >
             <div class="accordion-body">
               <strong>This is the third item's accordion body.</strong> It is
@@ -139,27 +239,10 @@ function About() {
           </div>
         </div>
       </div>
+
+      {/* ///////////////////////////////  FAQ   ///////////////////////////////////////////////*/}
     </div>
   );
 }
 
 export default About;
-
-// BLUF presentations take the Bottom Line Up Front approach, where the
-// presenter uses a simple structure to concisely present a topic. This
-// presenter has little time to research and prepare and can only use a
-// whiteboard marker to assist. BLUF presentations have a significantly
-// larger emphasis on the professional development of the presenter than the
-// technical information presented. This focuses on polishing valuable skills
-// for future professional presentations and creates a more casual atmosphere
-// to encourages audience interaction. actually sorry maybe this is better
-// BLUF presentations can be used to test your presentation skills by
-// presenting on a topic in which you have little prior knowledge and limited
-// time to prepare.  Presenting in such situations can be daunting but can
-// quickly develop your research, preparation and presentation skills. The
-// aim is for each session to be run only as fun learning exercises, with
-// topics coming from anything members are interested in. Simplicity in
-// presentation is also key and to minimise preparation time, each
-// presentation is restricted to a whiteboard and marker.  The BLUF group
-// aims to foster an encouraging environment where it is ok to be out of your
-// comfort zone and we all help each other improve.
